dc7578165fe1e1ff1951efb58c985475df06df1b
André de Mattos Ferraz <fandre@gmail.com>
2015-04-02 16:14:29 -0300

    cleaning main file

--- main.py
+++ main.py
@@ -7,28 +7,6 @@ from BagOfWords import BagOfWords
 from VectorSpaceModel import VectorSpaceModel
 from Document import Document
 
-'''
-def removePunctuation (text):
-    exclude = set(string.punctuation)
-    return ''.join(ch for ch in text if ch not in exclude)
-
-arrDocuments = [
-                removePunctuation("Site oficial da campanha de Dilma Rousseff a reeleicao presidencial. Dilma presidente".lower()) ,
-                removePunctuation("Conheca a biografia e trajetoria politica de Dilma Rousseff".lower()) ,
-                removePunctuation("Entenda por que Dilma Rousseff nao pode ser vista como faxineira da corrupcao".lower()) ,
-                removePunctuation("O governo Dilma Rousseff começou sob o signo da corrupcao".lower()) ,
-                removePunctuation("Federais desmentem Dilma no JN, e revelam interferencia do governo para reduzir combate à corrupcao".lower()) ,
-                removePunctuation("Compare as propostas de governo de Dilma e Aecio".lower()) ,
-                removePunctuation("Dilma uma cadeirante que precisa de sua ajuda".lower()) ,
-                removePunctuation("Aecio um cartola do futebol".lower())
-                ]
-
-bof = BagOfWords(arrDocuments)
-vsm = VectorSpaceModel(bof, arrDocuments)
-vsm.rankDocs("presidente dilma corrupcao")
-vsm.printRank()
-'''
-
 print "------------------------------------"
 
 bod = BagOfDocuments()
